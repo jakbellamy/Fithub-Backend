0af650ccead06e4cbf3418118dab0c7a4d391aad
Jakob Bellamy <jakbellamy@gmail.com>
2019-09-25 03:45:00 -0500

    login authenticatioin technically working. still need to be able to dispatch to nav and user, but am getting error

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,23 +4,24 @@ import {User} from '../models/userModel'
 import { authValidation } from '../validators/authValidation';
 
 export const authRequest = async (req, res) => {
-    // console.log(req.body)
+    console.log(req.body)
     const {error} = authValidation(req.body)
     if(error) {
-        return res.status(400).send({success: false})
+        return res.status(400).send({msg: error, success: false})
     }
 
     let user = await User.findOne({username: req.body.username})
     if(!user) {
-        return res.status(400).send({success: false})
+        return res.status(400).send({msg: "User Doesn't Exist", success: false})
     }
 
     const validPassword = await bcrypt.compare(req.body.password, user.password)
     if(!validPassword) {
-        return res.status(400).send({success: false})
+        return res.status(400).send({msg: 'Invalid Password', success: false})
     }
 
     const token = user.generateAuthToken()
-    res.send({token: token, user: user})
-    // console.log({token: token, user: user, success: true})
+    res.send({token: token, user: user, success: true})
+    console.log({token: token, user: user})
 }
+
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 import { authRequest } from "../controllers/authController";
+import {auth} from '../middleware/auth'
 
 const authRoutes = (app) => {
     app.route('/login')
     .post(authRequest);
+
+    app.route('/auth')
+    .post(auth)
 }
 
 export default authRoutes
